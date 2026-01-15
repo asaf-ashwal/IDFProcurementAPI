89039d23548a49f01228f7feefe17b7d700fb76c
asaf_ashwal <ashwalasafx@gmail.com>
2026-01-15 13:00:22 +0200

    commiting

--- src/budget/utils/functions.ts
+++ src/budget/utils/functions.ts
@@ -1,9 +1,8 @@
 import { existsSync } from 'fs';
-import mysql from "mysql2/promise";
+import mysql from 'mysql2/promise';
 
 // import {  } from "../../data";
-import { readFile, writeFile,  } from 'fs/promises';
-
+import { readFile, writeFile } from 'fs/promises';
 
 export async function reed_file() {
   try {
@@ -16,32 +15,26 @@ export async function reed_file() {
   }
 }
 
-export async function write_file(data, ) {
+export async function write_file(data) {
   await writeFile('./src/data/buget', JSON.stringify(data), 'utf8');
   return true;
 }
 
 export async function creatFiles() {
-  const res = await writeFile('./src/data/buget', JSON.stringify({currentBudget:0}), 'utf-8');
+  const res = await writeFile(
+    './src/data/buget',
+    JSON.stringify({ currentBudget: 0 }),
+    'utf-8',
+  );
   return res;
 }
 
 export async function creatDB() {
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "rootpass123",
-  // database: "schoolSystem",
-})
-await pool.execute('create DATABASE if not EXISTS IDF_Procurement_API')
+  const pool = mysql.createPool({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'rootpass123',
+    database: 'IDF_Procurement_API',
+  });
+  await pool.execute('create DATABASE if not EXISTS IDF_Procurement_API');
 }
-
-// function createfile() {
-
-// }
-// function read(params:type) {
-
-// }
-// function write(params:type) {
-
-// }
